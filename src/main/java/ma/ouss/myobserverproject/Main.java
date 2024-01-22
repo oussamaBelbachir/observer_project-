b0473d8db13b682e2e1c7134351a31ded65c74cb
oussamaBelbachir <oussamabelbachir60@gmail.com>
2024-01-22 23:32:59 +0100

    observer project my second commit

--- src/main/java/ma/ouss/myobserverproject/Main.java
+++ src/main/java/ma/ouss/myobserverproject/Main.java
@@ -3,8 +3,5 @@ package ma.ouss.myobserverproject;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
